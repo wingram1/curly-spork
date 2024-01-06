d3861198cfb797a51bd3f90cb5e0bb4637ff0dbb
wingram1 <waingram96@gmail.com>
2024-01-05 22:42:16 -0500

    move to bottom right

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,41 @@
 const main = document.querySelector("#main");
-const p = document.querySelector("#player");
-const g = document.querySelector("#goal");
+const player = document.querySelector("#player");
+const goal = document.querySelector("#goal");
 
+const p = {
+  x: 0,
+  y: 0,
+};
+
+const speed = 4;
+
+function handleMove(e) {
+  console.log(e.key);
+
+  switch (e.key) {
+    case "w":
+      p.y += speed;
+      document.querySelector("#player").style.top = `${p.y}px`;
+      break;
+    case "a":
+      p.x -= speed;
+      document.querySelector("#player").style.left = `${p.x}px`;
+      break;
+    case "s":
+      p.y += speed;
+      document.querySelector("#player").style.top = `${p.y}px`;
+      break;
+    case "d":
+      p.x += speed;
+      document.querySelector("#player").style.left = `${p.x}px`;
+      break;
+    default:
+      console.log("use wasd dumbass");
+  }
+
+  if (p.x == 448 && p.y == 448) {
+    main.textContent = "COMPLETE\n\nGOOD JOB\n:)";
+  }
+}
+
+document.addEventListener("keypress", handleMove);
